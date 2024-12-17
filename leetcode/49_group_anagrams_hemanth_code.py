7d572756a104c8cdc5db3dbf8e4c21a0db315dfc
Hemanth <81924396+hemanth-sani@users.noreply.github.com>
2024-12-16 22:23:02 -0500

    Added sort string function

--- leetcode/49_group_anagrams_hemanth_code.py
+++ leetcode/49_group_anagrams_hemanth_code.py
@@ -1,12 +1,26 @@
 class Solution:
+    def sort_string(self, word):
+        alphabet_count = [0] * 26
+        
+        sorted_string = ''
+        for char in word.replace(" ",""):
+            alphabet_count[ord(char)-ord('a')] += 1
+        for alphabet_index, count in enumerate(alphabet_count):
+            if count > 0:
+               sorted_string += chr(alphabet_index + ord('a')) * count
+        return sorted_string
+
+    
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         group_strings = {}
-        first_string = sorted(strs[0])
-        first_string = ''.join(first_string)
-        group_strings[first_string] = []
+        first_string = self.sort_string(strs[0])
+        for i in strs[0]:
+
+            first_string = ''.join(first_string)
+            group_strings[first_string] = []
         
         for i in range(len(strs)):
-            sorted_str = sorted(strs[i])
+            sorted_str = self.sort_string(strs[i])
             sorted_str = ''.join(sorted_str)
             if sorted_str in group_strings:
                 group_strings[sorted_str].append(strs[i])
@@ -14,3 +28,4 @@ class Solution:
                 group_strings[sorted_str] = []
                 group_strings[sorted_str].append(strs[i])
         return list(group_strings.values())
+ 
