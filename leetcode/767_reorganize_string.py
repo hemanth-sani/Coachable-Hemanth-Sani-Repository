8935d7dee9e3af62b5fdf5723555b85ddb691174
Hemanth <81924396+hemanth-sani@users.noreply.github.com>
2024-12-16 22:48:34 -0500

    Used defautdict 767_reorganize_string.py

--- leetcode/767_reorganize_string.py
+++ leetcode/767_reorganize_string.py
@@ -1,25 +1,32 @@
-from collections import Counter
+from collections import defaultdict
 import heapq
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        charCount = Counter(s)
-        maxFreqCount = []
-        for word, count in charCount.items():
-            heapq.heappush(maxFreqCount, [-count, word])
+
+        char_count = defaultdict()
+        for letter in s:
+            if letter not in char_count:
+                char_count[letter] = 1
+            else:
+                char_count[letter] += 1
+        print(char_count) 
+ 
+        max_freq_count = []
+        for word, count in char_count.items():
+            heapq.heappush(max_freq_count, [-count, word])
         
         prev = None
-        stringHolder = ''
-        while maxFreqCount or prev:
-            if prev and not maxFreqCount:
+        string_holder = ''
+        while max_freq_count or prev:
+            if prev and not max_freq_count:
                 return ""
-            count, word = heapq.heappop(maxFreqCount)
-            stringHolder += word
+            count, word = heapq.heappop(max_freq_count)
+            string_holder += word
             count += 1
             if prev:
-                heapq.heappush(maxFreqCount,prev)
+                heapq.heappush(max_freq_count,prev)
                 prev = None
             if count != 0:
                 prev = [count, word]
         
-        return stringHolder
-        
+        return string_holder
