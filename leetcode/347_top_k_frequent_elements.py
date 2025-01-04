c2d4458c218c77464acc1c1e7eb935700149f94f
Hemanth <81924396+hemanth-sani@users.noreply.github.com>
2025-01-04 16:35:53 -0500

    Update 347_top_k_frequent_elements.py

--- leetcode/347_top_k_frequent_elements.py
+++ leetcode/347_top_k_frequent_elements.py
@@ -1,21 +1,21 @@
 import heapq
-
+from collections import defaultdict
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count_of_nums = {}
+        count_of_nums = defaultdict(int)
         for num in nums:
             if num in count_of_nums:
                 count_of_nums[num] += 1
             else:
                 count_of_nums[num] = 1
 
-        top_k_elements = []
+        max_freq_heap = []
         for num, frequency in count_of_nums.items():
-            if len(top_k_elements) >= k:
-                heapq.heappushpop(top_k_elements, (frequency, num))
+            if len(max_freq_heap) >= k:
+                heapq.heappushpop(max_freq_heap, (frequency, num))
             else:
-                heapq.heappush(top_k_elements, (frequency, num))
+                heapq.heappush(max_freq_heap, (frequency, num))
         result = []
-        for frequency, num in top_k_elements:
+        for frequency, num in max_freq_heap:
             result.append(num)
         return result
