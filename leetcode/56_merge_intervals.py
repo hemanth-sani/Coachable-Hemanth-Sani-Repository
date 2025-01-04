223f67448ba18ccf6d74cf86a5d5d34681decfa5
Hemanth <81924396+hemanth-sani@users.noreply.github.com>
2025-01-04 16:43:36 -0500

    Update 56_merge_intervals.py

--- leetcode/56_merge_intervals.py
+++ leetcode/56_merge_intervals.py
@@ -7,15 +7,14 @@ class Solution:
         start, end = sorted_intervals[0]
         
         while i < N:
-            next_start = sorted_intervals[i][0]
-            next_end = sorted_intervals[i][1]
+            next_start, next_end = sorted_intervals[i]
             if end >= next_start:
                 max_interval_value = max(end, next_end)
                 min_interval_value = min(start, next_start)
                 start, end = min_interval_value, max_interval_value
             else:
                 result.append([start, end])
-                start, end = sorted_intervals[i][0], sorted_intervals[i][1]
+                start, end = sorted_intervals[i]
 
             i += 1
 
