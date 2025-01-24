c06528dd22b369783b4eac14144ece0cfb8257d7
Hemanth <81924396+hemanth-sani@users.noreply.github.com>
2025-01-23 21:55:18 -0500

    Update 22_generate_parentheses.py

--- leetcode/22_generate_parentheses.py
+++ leetcode/22_generate_parentheses.py
@@ -3,20 +3,20 @@ class Solution:
         res = []
         stack = []
         
-        def parenthesisFormation(open_count, close_count):
+        def parenthesis_formation(open_count, close_count):
 
             if open_count == close_count == n:
                 res.append("".join(stack))
             
             if open_count < n:
                 stack.append("(")
-                parenthesisFormation(open_count + 1, close_count)
+                parenthesis_formation(open_count + 1, close_count)
                 stack.pop()
             
             if close_count < open_count:
                 stack.append(")")
-                parenthesisFormation(open_count, close_count + 1)
+                parenthesis_formation(open_count, close_count + 1)
                 stack.pop()
         
-        parenthesisFormation(0,0)
+        parenthesis_formation(0,0)
         return res
